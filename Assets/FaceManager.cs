243c10353abe15d15cdd7eb3d3c10f7da69252c7
Nancy <123rust345@gmail.com>
2026-01-31 09:57:49 -0500

    Polished Suspect and Face Prefabs

--- Assets/FaceManager.cs
+++ Assets/FaceManager.cs
@@ -10,9 +10,10 @@ public class FaceManager : MonoBehaviour
     [SerializeField] private static List<Sprite> ears = new List<Sprite>();
     [SerializeField] private static List<Sprite> hair = new List<Sprite>();
     [SerializeField] private static List<Sprite> accessories = new List<Sprite>();
-    [SerializeField] private static List<Sprite> facialHair = new List<Sprite>();
     [SerializeField] private static List<Sprite> eyebrows = new List<Sprite>();
     [SerializeField] private static List<Sprite> noses = new List<Sprite>();
+    [SerializeField] private static List<Sprite> beard = new List<Sprite>();
+    [SerializeField] private static List<Sprite> moustache = new List<Sprite>();
 
     [Header("Selected Face Parts")]
     [SerializeField] private Sprite selectedEyes;
@@ -20,7 +21,8 @@ public class FaceManager : MonoBehaviour
     [SerializeField] private Sprite selectedEars;
     [SerializeField] private Sprite selectedHair;
     [SerializeField] private Sprite selectedAccessory;
-    [SerializeField] private Sprite selectedFacialHair;
+    [SerializeField] private Sprite selectedBeard;
+    [SerializeField] private Sprite selectedMoustache;
     [SerializeField] private Sprite selectedEyebrows;
     [SerializeField] private Sprite selectedNose;
 
@@ -30,7 +32,8 @@ public class FaceManager : MonoBehaviour
     [SerializeField] private Image earsRenderer;
     [SerializeField] private Image hairRenderer;
     [SerializeField] private Image accessoryRenderer;
-    [SerializeField] private Image facialHairRenderer;
+    [SerializeField] private Image beardRenderer;
+    [SerializeField] private Image moustacheRenderer;
     [SerializeField] private Image eyebrowsRenderer;
     [SerializeField] private Image noseRenderer;
 
@@ -44,6 +47,7 @@ public class FaceManager : MonoBehaviour
     void Awake()
     {
         EnsureFacePartsLoaded();
+        ResolveRenderers();
 
         if (generate)
         {
@@ -71,9 +75,29 @@ public class FaceManager : MonoBehaviour
         ears = new List<Sprite>(Resources.LoadAll<Sprite>("Faces/Ears"));
         hair = new List<Sprite>(Resources.LoadAll<Sprite>("Faces/Hair"));
         accessories = new List<Sprite>(Resources.LoadAll<Sprite>("Faces/Accessories"));
-        facialHair = new List<Sprite>(Resources.LoadAll<Sprite>("Faces/FacialHair"));
+        beard = new List<Sprite>(Resources.LoadAll<Sprite>("Faces/Beard"));
+        moustache = new List<Sprite>(Resources.LoadAll<Sprite>("Faces/Moustache"));
         eyebrows = new List<Sprite>(Resources.LoadAll<Sprite>("Faces/Eyebrows"));
         noses = new List<Sprite>(Resources.LoadAll<Sprite>("Faces/Nose"));
+    }
+
+    void ResolveRenderers()
+    {
+        eyesRenderer = FindImageByName("Eyes");
+        mouthRenderer = FindImageByName("Mouth");
+        earsRenderer = FindImageByName("Ears");
+        hairRenderer = FindImageByName("Hair");
+        accessoryRenderer = FindImageByName("Accessories");
+        beardRenderer = FindImageByName("Beard");
+        moustacheRenderer = FindImageByName("Moustache");
+        eyebrowsRenderer = FindImageByName("Eyebrows");
+        noseRenderer = FindImageByName("Nose");
+    }
+
+    Image FindImageByName(string objectName)
+    {
+        var go = GameObject.Find(objectName);
+        return go != null ? go.GetComponent<Image>() : null;
     }
 
     public void GenerateFace(int? seed)
@@ -96,7 +120,8 @@ public class FaceManager : MonoBehaviour
         selectedEars = ears.Count > 0 ? ears[Random.Range(0, ears.Count)] : null;
         selectedHair = hair.Count > 0 ? hair[Random.Range(0, hair.Count)] : null;
         selectedAccessory = accessories.Count > 0 ? accessories[Random.Range(0, accessories.Count)] : null;
-        selectedFacialHair = facialHair.Count > 0 ? facialHair[Random.Range(0, facialHair.Count)] : null;
+        selectedBeard = beard.Count > 0 ? beard[Random.Range(0, beard.Count)] : null;
+        selectedMoustache = moustache.Count > 0 ? moustache[Random.Range(0, moustache.Count)] : null;
         selectedEyebrows = eyebrows.Count > 0 ? eyebrows[Random.Range(0, eyebrows.Count)] : null;
         selectedNose = noses.Count > 0 ? noses[Random.Range(0, noses.Count)] : null;
 
@@ -105,7 +130,8 @@ public class FaceManager : MonoBehaviour
         earsRenderer.sprite = selectedEars;
         hairRenderer.sprite = selectedHair;
         accessoryRenderer.sprite = selectedAccessory;
-        facialHairRenderer.sprite = selectedFacialHair;
+        //beardRenderer.sprite = selectedBeard;
+        //moustacheRenderer.sprite = selectedMoustache;
         eyebrowsRenderer.sprite = selectedEyebrows;
         noseRenderer.sprite = selectedNose;
 
