cfdd5f3eb540788cd4575b9b3e329aedee762e1d
Ben <bengg2015@gmail.com>
2026-01-30 20:20:09 -0500

    Made Basic Face Generating System

---
+++ Assets/FaceManager.cs
@@ -0,0 +1,79 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceManager : MonoBehaviour
+{
+    private static bool isLoaded;
+
+    [Header("Loaded Face Parts")]
+    [SerializeField] private static List<Sprite> heads = new List<Sprite>();
+    [SerializeField] private static List<Sprite> eyes = new List<Sprite>();
+    [SerializeField] private static List<Sprite> mouths = new List<Sprite>();
+    [SerializeField] private static List<Sprite> ears = new List<Sprite>();
+    [SerializeField] private static List<Sprite> hair = new List<Sprite>();
+    [SerializeField] private static List<Sprite> accessories = new List<Sprite>();
+    [SerializeField] private static List<Sprite> facialHair = new List<Sprite>();
+    [SerializeField] private static List<Sprite> eyebrows = new List<Sprite>();
+
+    [Header("Selected Face Parts")]
+    [SerializeField] private Sprite selectedHead;
+    [SerializeField] private Sprite selectedEyes;
+    [SerializeField] private Sprite selectedMouth;
+    [SerializeField] private Sprite selectedEars;
+    [SerializeField] private Sprite selectedHair;
+    [SerializeField] private Sprite selectedAccessory;
+    [SerializeField] private Sprite selectedFacialHair;
+    [SerializeField] private Sprite selectedEyebrows;
+
+    [Header("Face Part Renderers")]
+    [SerializeField] private SpriteRenderer headRenderer;
+    [SerializeField] private SpriteRenderer eyesRenderer;
+    [SerializeField] private SpriteRenderer mouthRenderer;
+    [SerializeField] private SpriteRenderer earsRenderer;
+    [SerializeField] private SpriteRenderer hairRenderer;
+    [SerializeField] private SpriteRenderer accessoryRenderer;
+    [SerializeField] private SpriteRenderer facialHairRenderer;
+    [SerializeField] private SpriteRenderer eyebrowsRenderer;
+
+    void Awake()
+    {
+        heads = new List<Sprite>(Resources.LoadAll<Sprite>("Faces/Head"));
+        eyes = new List<Sprite>(Resources.LoadAll<Sprite>("Faces/Eyes"));
+        mouths = new List<Sprite>(Resources.LoadAll<Sprite>("Faces/Mouth"));
+        ears = new List<Sprite>(Resources.LoadAll<Sprite>("Faces/Ears"));
+        hair = new List<Sprite>(Resources.LoadAll<Sprite>("Faces/Hair"));
+        accessories = new List<Sprite>(Resources.LoadAll<Sprite>("Faces/Accessories"));
+        facialHair = new List<Sprite>(Resources.LoadAll<Sprite>("Faces/FacialHair"));
+        eyebrows = new List<Sprite>(Resources.LoadAll<Sprite>("Faces/Eyebrows"));
+
+        if (headRenderer == null) headRenderer = gameObject.AddComponent<SpriteRenderer>();
+        if (eyesRenderer == null) eyesRenderer = gameObject.AddComponent<SpriteRenderer>();
+        if (mouthRenderer == null) mouthRenderer = gameObject.AddComponent<SpriteRenderer>();
+        if (earsRenderer == null) earsRenderer = gameObject.AddComponent<SpriteRenderer>();
+        if (hairRenderer == null) hairRenderer = gameObject.AddComponent<SpriteRenderer>();
+        if (accessoryRenderer == null) accessoryRenderer = gameObject.AddComponent<SpriteRenderer>();
+        if (facialHairRenderer == null) facialHairRenderer = gameObject.AddComponent<SpriteRenderer>();
+        if (eyebrowsRenderer == null) eyebrowsRenderer = gameObject.AddComponent<SpriteRenderer>();
+    }
+
+    public void GenerateFace()
+    {
+        selectedHead = heads.Count > 0 ? heads[Random.Range(0, heads.Count)] : null;
+        selectedEyes = eyes.Count > 0 ? eyes[Random.Range(0, eyes.Count)] : null;
+        selectedMouth = mouths.Count > 0 ? mouths[Random.Range(0, mouths.Count)] : null;
+        selectedEars = ears.Count > 0 ? ears[Random.Range(0, ears.Count)] : null;
+        selectedHair = hair.Count > 0 ? hair[Random.Range(0, hair.Count)] : null;
+        selectedAccessory = accessories.Count > 0 ? accessories[Random.Range(0, accessories.Count)] : null;
+        selectedFacialHair = facialHair.Count > 0 ? facialHair[Random.Range(0, facialHair.Count)] : null;
+        selectedEyebrows = eyebrows.Count > 0 ? eyebrows[Random.Range(0, eyebrows.Count)] : null;
+
+        headRenderer.sprite = selectedHead;
+        eyesRenderer.sprite = selectedEyes;
+        mouthRenderer.sprite = selectedMouth;
+        earsRenderer.sprite = selectedEars;
+        hairRenderer.sprite = selectedHair;
+        accessoryRenderer.sprite = selectedAccessory;
+        facialHairRenderer.sprite = selectedFacialHair;
+        eyebrowsRenderer.sprite = selectedEyebrows;
+    }
+}
